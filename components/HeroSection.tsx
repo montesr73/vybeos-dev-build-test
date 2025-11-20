426d32df59a6c2a596a30c0a0a0653157b6af9e2
montes.r0703@gmail.com <ricardo.montes@orbee.com>
2025-11-20 09:48:48 -0800

    task(`HeroSection.tsx`): Add margin to bottom of compnent.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,8 @@
 // components/HeroSection.tsx
 export default function HeroSection() {
   return (
-    <section className="w-full max-w-5xl mx-auto text-center px-6 md:px-8 mt-24">
-      <h2 className="font-pp-machina font-semibold text-display-md md:text-display-md lg:text-display-md text-white text-[36px]">
+    <section className="w-full max-w-5xl mx-auto text-center px-6 md:px-8 mt-24 mb-16">
+      <h2 className="mb-6 font-pp-machina font-semibold text-display-md md:text-display-md lg:text-display-md text-white text-[36px]">
         How It Works
       </h2>
       <p className="mt-4 max-w-xl mx-auto font-pp-montreal text-body-md text-neutral-muted text-center text-[16px]">
